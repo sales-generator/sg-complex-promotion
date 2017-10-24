34a3650fd6f816574db211366bf8445863f418a8
sales-generator <pashebor@salesgenerator.pro>
2017-10-24 19:00:30 +0300

    Done action date circle and performed some fixes in advisory popup

--- app/components/main/banner/Banner.jsx
+++ app/components/main/banner/Banner.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import {showBanner} from '../../../actions/index';
 import Iframe from 'react-iframe';
+import {daysInMonth, findClosestDay, closestValue, monthNames} from '../../../utils/time.date';
 
 class Banner extends React.Component{
 
@@ -13,11 +14,15 @@ class Banner extends React.Component{
     }
 
     render() {
+        let currentDate = new Date();
+        let daysCount = daysInMonth(currentDate.getYear(), currentDate.getMonth());
+        let fiveDaysArray = findClosestDay(daysCount), actionDay = closestValue(fiveDaysArray, currentDate.getDate()),
+            currentMonthName = monthNames();
         return(
             <div className="popup-overlay popup-overlay--banner">
                 <div className="popup-form popup-form--banner">
                     <div className="popup-form__close" onClick={this.closeBannerPopup.bind(this)}>&times;</div>
-                    <p className="banner-text">Оставьте заявку до <span>25 октября</span></p>
+                    <p className="banner-text">Оставьте заявку до <span>{actionDay} {currentMonthName}</span></p>
                     <p className="banner-text">и получите аудит вашего сайта</p>
                     <p className="banner-text"><span>в подарок</span><img src="images/gift.png"/></p>
                     <Iframe url="https://gpro.bitrix24.ru/pub/form.php?view=frame&form_id=47&widget_user_lang=ru&sec=epk8ee&r=1508763826284#%7B%22domain%22%3A%22file%3A%2F%2F%22%2C%22from%22%3A%22file%3A%2F%2F%2FC%3A%2FUsers%2FPashebor%2FDesktop%2F%25D0%259D%25D0%25BE%25D0%25B2%25D1%258B%25D0%25B9%2520%25D1%2582%25D0%25B5%25D0%25BA%25D1%2581%25D1%2582%25D0%25BE%25D0%25B2%25D1%258B%25D0%25B9%2520%25D0%25B4%25D0%25BE%25D0%25BA%25D1%2583%25D0%25BC%25D0%25B5%25D0%25BD%25D1%2582.html%22%7D"
