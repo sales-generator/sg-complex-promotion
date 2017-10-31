a588989409cb1de47950ba9a8f195a4afce6da3e
Yulia777 <1993belochka1993@list.ru>
2017-10-31 16:04:51 +0300

    The LiveRequest added + 1 a month if a date = 31

--- app/utils/time.date.js
+++ app/utils/time.date.js
@@ -1,14 +1,15 @@
-export const  daysInMonth = (month,year) => new Date(year, month, 0).getDate();
+export const  daysInMonth = (month, year) => new Date(year, month+1, 0).getDate();
 export const findClosestDay = dayscount => {
     let daysArray = [30];
     let daysCount = 0;
-    if (dayscount > 30) {
-        daysCount = (dayscount - 1) / 5;
+    if (dayscount === 31) {
+        daysCount = 30 / 5;
+        dayscount = dayscount - 1;
         for (let i = 0; i < daysCount; i += 1) {
             dayscount = dayscount - 5;
             daysArray.push(dayscount);
         }
-    } else {
+     } else {
         daysCount = dayscount / 5;
         for (let i = 0; i < daysCount; i += 1) {
             dayscount = dayscount - 5;
@@ -30,14 +31,64 @@ export const closestValue =(array, value) => {
     return closest; // return the value
 };
 
-export const monthNames = () => {
+export const monthNames = (days) => {
     let monthsArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
     let currentMonthNumber = new Date().getMonth();
     let monthName = '';
+    if (days === 31) {
+        monthsArray.forEach((month, i, array) => {
+            if (i === currentMonthNumber) {
+                monthName = array[i + 1];
+            }
+        });
+    } else {
+        monthsArray.forEach((month, i) => {
+            if (i === currentMonthNumber) {
+                monthName = month;
+            }
+        });
+    }
+
+    return monthName;
+};
+
+/*
+export const monthNamesLastDay = (days) => {
+    let  monthsArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+    let currentMonthNumber = new Date().getMonth();
+    let currentMonthNumber2 = new Date().getMonth()+1;
+    let currentDayNumber = new Date().getDate();
+    let monthName = '';
+    if (currentDayNumber === 31) {
     monthsArray.forEach((month, i) => {
-        if (i === currentMonthNumber) {
+        if (i === currentMonthNumber2) {
             monthName = month;
         }
     });
+    } if (currentDayNumber < 31) {
+        monthsArray.forEach((month, i) => {
+            if (i === currentMonthNumber) {
+                monthName = month;
+            }
+        });
+    }
+    /!*daysArray.forEach((daysCount, monthsArray) => {
+        if (daysCount == 31) {
+            monthName = monthsArray + 2;
+        }
+    });*!/
+    /!*monthsArray.forEach((month, i) => {
+        if (i === currentMonthNumber) {
+            monthName = month;
+        }
+    });*!/
+
+        /!*if (currentDayNumber === 31) {
+            monthName = currentMonthNumber2;
+            console.log(currentDayNumber + ' это тут')
+        } else {
+            monthName = currentMonthNumber;
+        }*!/
+
     return monthName;
-};
+};*/
