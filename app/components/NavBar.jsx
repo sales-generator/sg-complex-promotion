6c1885b21e8e059696f372c9f81fbd9777e870f6
sales-generator <pashebor@salesgenerator.pro>
2017-12-04 16:12:14 +0300

    Header screen has been remastered and the employee slider was plugged

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import {showModal} from '../actions/index';
 import { bindActionCreators } from 'redux'
 import {connect} from 'react-redux';
@@ -14,15 +13,15 @@ class NavBar extends Component{
 
     render() {
         return(
-          <nav className="container nav">
-              <div className="nav__logo"><a href="http://sales-generator.ru"><img src="images/saleslogowhite.svg"  alt="Sales Generator logo"/></a></div>
-              <h2 className="nav__title">Агентство интернет-маркетинга</h2>
-              <div className="nav__tel">
-                  <a  href="tel:88003331293">8 (800) 333 12 93</a>
-                  <a  href="tel:84951250840">8 (495) 125 08 40</a>
-              </div>
-              <button className="btn btn--callback" onClick={this.openModalHandler.bind(this)}>Заказать звонок</button>
-          </nav>
+            <nav className="container nav">
+                <div className="nav__logo"><a href="http://sales-generator.ru"><img src="images/logo-black.png"  alt="Sales Generator logo"/></a></div>
+                <h2 className="nav__title">Агентство интернет-маркетинга</h2>
+                <div className="nav__tel">
+                    <a  href="tel:88003331293">8 (800) 333 12 93</a>
+                    <a  href="tel:84951250840">8 (495) 125 08 40</a>
+                </div>
+                <button className="btn btn--callback" onClick={this.openModalHandler.bind(this)}>Заказать звонок</button>
+            </nav>
         );
     };
 }
